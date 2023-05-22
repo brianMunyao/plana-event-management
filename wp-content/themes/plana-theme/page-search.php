9e348afc0da38e868ea91a0c934da2e7fe1f1ccc
brianMunyao <brianmunyao6@gmail.com>
2023-05-22 23:30:30 +0300

    added search function

--- wp-content/themes/plana-theme/page-search.php
+++ wp-content/themes/plana-theme/page-search.php
@@ -2,41 +2,43 @@
 
 /**
  * 
- *  Template Name: Home Template
+ * Template Name: Search Page
  */
-
-get_header();
 ?>
 
+<?php get_header() ?>
+
 <?php
+$query = $_GET['q'] ?? '';
+
 global $wpdb;
 
 $events_table = $wpdb->prefix . 'events';
-$events = $wpdb->get_results("SELECT * FROM $events_table");
+$events = $wpdb->get_results("SELECT * FROM $events_table WHERE e_name LIKE '%$query%' OR e_location LIKE '%$query%'");
+
+
 ?>
 
-<div class="home-container">
-    <form action="<?php echo site_url('/search') ?>" method="get">
-        <div class="search-section">
-            <div class="search-section-inner">
-                <h2>Discover Events Near You</h2>
+<div class="search-page-con">
+    <form action="" method="get">
 
-                <div class="search-con">
-                    <ion-icon name="search-outline"></ion-icon>
-                    <input type="search" name="q" id="search" placeholder="Search by location or event name">
-                    <button class="custom-btn" type="submit">SEARCH</button>
-                </div>
-            </div>
+        <div class="search-con">
+            <ion-icon name="search-outline"></ion-icon>
+            <input type="search" name="q" id="search" value="<?php echo $_GET['q'] ?>" placeholder="Search by location or event name">
+            <button class="custom-btn" type="submit">SEARCH</button>
         </div>
     </form>
 
-    <div class="events-list-con">
-        <h2>Upcoming Events</h2>
+    <p class="search-results">
+        <?php echo count($events) . " result(s) found"; ?>
+    </p>
 
-        <div class="events-list">
-            <?php
+    <div class="events-list">
+        <?php
+        if (count($events) > 0) {
+
             foreach ($events as $event) {
-            ?>
+        ?>
                 <a href='<?php echo site_url("/event?id={$event->e_id}"); ?>'>
                     <div class="event-card">
                         <div class="event-top" style="background: url('<?php echo $event->e_image_url; ?>');background-size: cover;background-position: center;">
@@ -63,10 +65,15 @@ $events = $wpdb->get_results("SELECT * FROM $events_table");
                 </a>
             <?php
             }
+        } else {
             ?>
-        </div>
+            <p class="no-results">
+                No results matching that query
+            </p>
+        <?php
+        }
+        ?>
     </div>
 </div>
 
-
-<?php get_footer(); ?>
+<?php get_footer() ?>
