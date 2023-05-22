4a61f02d9f42c513eea2bf1d5bdf5b8085051ba9
brianMunyao <brianmunyao6@gmail.com>
2023-05-22 21:24:50 +0300

    updated: login and register functionality

--- wp-content/themes/plana-theme/page-login.php
+++ wp-content/themes/plana-theme/page-login.php
@@ -1,39 +1,30 @@
 <?php
+if (is_user_logged_in()) wp_redirect(home_url());
 /*
+
 Template Name: Login Page
 */
-get_header();
-?>
 
-<?php
-if (isset($_POST['submit'])) {
-    require_once('wp-load.php');
+
+if (isset($_POST['login-submit'])) {
 
     // Sanitize and validate user input
-    $username = sanitize_text_field($_POST['fullname']);
     $email = sanitize_text_field($_POST['email']);
     $password = sanitize_text_field($_POST['password']);
 
-    // Perform login
-    $login_data = array(
-        'user_login' => $fullname,
-        'user_email'  => $email,
+    $user_verify = wp_signon([
+        'user_login' => $email,
         'user_password' => $password,
         'remember' => true,
-    );
+    ]);
 
-    $user_verify = wp_signon($login_data, false);
-
-    if (!is_wp_error($user_verify)) {
-        // Redirect to the home page or any desired page after successful login
-        wp_redirect(home_url());
-        exit;
-    } else {
-        $error_message = $user_verify->get_error_message();
-        echo "Login failed: " . $error_message;
+    if (is_wp_error($user_verify)) {
+        $error_msg = $user_verify->get_error_message();
     }
 }
 ?>
+
+<?php get_header(); ?>
 
 <div class="form-container">
     <div class="regcover">
@@ -41,24 +32,27 @@ if (isset($_POST['submit'])) {
             <h1>Plana</h1>
             <P>Welcome back to Plana. Your number one event management system.</P>
         </div>
-        <form action="">
+        <form action="" method="POST">
             <div class="form">
-                    <h2>Login</h2>
-                    <div class="input1">
-                        <label for="">Email Address</label>
-                        <div class="icons1">
-                            <ion-icon name="mail-outline"></ion-icon>
-                            <input type="text" placeholder="Enter email address" name="email">
-                        </div>
+                <h2>Login</h2>
+
+                <p class="error"><?php echo $error_msg ?></p>
+
+                <div class="input1">
+                    <label for="">Email Address</label>
+                    <div class="icons1">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="text" placeholder="Enter email address" name="email" required>
                     </div>
-                    <div class="input1">
-                        <label for="">Password</label>
-                        <div class="icons1">
-                            <ion-icon name="lock-open-outline"></ion-icon>
-                            <input type="password" placeholder="Enter password" name="password">
-                        </div>
+                </div>
+                <div class="input1">
+                    <label for="">Password</label>
+                    <div class="icons1">
+                        <ion-icon name="lock-open-outline"></ion-icon>
+                        <input type="password" placeholder="Enter password" name="password" required>
                     </div>
-                    <button class="btnreg" name="submit">Login</button>
+                </div>
+                <button type="submit" class="btnreg" name="login-submit">Login</button>
             </div>
         </form>
     </div>
